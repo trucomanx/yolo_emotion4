18d5c8a415c4614cb9d30141d45771593d572803
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-05-13 15:42:15 -0300

    Update main.py

--- yolo-cls-train/main.py
+++ yolo-cls-train/main.py
@@ -23,3 +23,32 @@ else:
     print(results)
 
 
+def verify_categorical_accuracy(model,input_dir,labels_dic={0: 'negative', 1: 'neutro', 2: 'pain', 3: 'positive'}):
+    L=0;
+    Count=0;
+    for ID, LABEL in labels_dic.items():
+        dir_path=os.path.join(input_dir,LABEL);
+        res=model(dir_path);
+        L=L+len(res);
+        for dat in res:
+            if ID==dat.probs.top1:
+                Count=Count+1;
+    return Count*1.0/L, L;    
+        
+model=model.load('runs/classify/train/weights/best.pt');
+
+train_acc, L_train = verify_categorical_accuracy(model,os.path.join(dataset_path,'train'));
+val_acc  , L_val   = verify_categorical_accuracy(model,os.path.join(dataset_path,'val'));
+test_acc , L_test  = verify_categorical_accuracy(model,os.path.join(dataset_path,'test'));
+
+data=dict();
+data['train_categorical_accuracy']=train_acc;
+data['val_categorical_accuracy']  =val_acc;
+data['test_categorical_accuracy'] =test_acc;
+data['train_length']=L_train;
+data['val_length']=L_val;
+data['test_length']=L_test;
+
+import json
+with open("statistics.json", "w") as write_file:
+    json.dump(student, write_file, indent=4)
